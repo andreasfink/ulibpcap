42f0bd3451050c2b487cbf17873a9fa9101732c8
Andreas Fink <andreas@fink.org>
2017-02-09 08:40:32 +0100

    cleanup of copyright lines

--- dump-sccp/main.m
+++ dump-sccp/main.m
@@ -3,7 +3,7 @@
 //  dump-sccp
 //
 //  Created by Andreas Fink on 19.07.16.
-//  Copyright © 2016 Andreas Fink. All rights reserved.
+//  Copyright © 2017 Andreas Fink (andreas@fink.org). All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- ulibpcap/ulibpcap.h
+++ ulibpcap/ulibpcap.h
@@ -3,7 +3,7 @@
 //  ulibpcap
 //
 //  Created by Andreas Fink on 25.05.16.
-//  Copyright © 2016 Andreas Fink. All rights reserved.
+//  Copyright © 2017 Andreas Fink (andreas@fink.org). All rights reserved.
 //
 
 #import <ulib/ulib.h>

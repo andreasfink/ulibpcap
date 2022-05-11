64ea931c12dd9a35fab1031bffbc13077986a516
Andreas Fink <andreas@fink.org>
2022-05-11 23:49:14 +0200

    wip

--- pcap-test/main.m
+++ pcap-test/main.m
@@ -14,14 +14,112 @@ int main(int argc, const char * argv[])
 {
     @autoreleasepool
     {
+        uint8_t data[] = { 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x0A };
         
+        uint8_t srcMac[] = { 0x70,     0xB3,     0xD5,     0x23,     0xB0,     0x00,};
+        uint8_t dstMac[] = { 0x70,     0xB3,    0xD5,     0x23,     0xB0,     0x06, };
+
+        NSData *tcpPayload = [NSData dataWithBytes:data length:sizeof(data)];
+        NSData *srcMacAddr = [NSData dataWithBytes:srcMac length:sizeof(srcMac)];
+        NSData *dstMacAddr = [NSData dataWithBytes:dstMac length:sizeof(dstMac)];
+
+
+
+        NSData *sctpDataChunk = [UMPCAPMirrorPort sctpChunk:tcpPayload
+                                                      flags:0
+                                                        tsn:999
+                                                     stream:1
+                                       streamSequenceNumber:222
+                                          protocolIdentifier:5];
+        NSArray *a = @[sctpDataChunk];
+        
+        NSData *sctpData = [UMPCAPMirrorPort sctpPacket:a
+                                             sourcePort:2000
+                                        destinationPort:2001
+                                        verificationTag:99199];
+
+        NSData *udpData = [UMPCAPMirrorPort udpPacket:tcpPayload
+                                        sourceAddress:@"10.10.10.10"
+                                   destinationAddress:@"20.20.20.20"
+                                            sourcePort:199
+                                       destinationPort:199];
+        
+        NSData *tcpData = [UMPCAPMirrorPort tcpPacket:tcpPayload
+                                        sourceAddress:@"10.10.10.10"
+                                   destinationAddress:@"20.20.20.20"
+                                            sourcePort:65000
+                                       destinationPort:23
+                                        sequenceNumber:123
+                                             ackNumber:321
+                                                 flags:0
+                                            windowSize:512
+                                         urgentPointer:0
+                                        fragmentLength:0];
+
+        NSData *ipData1 = [UMPCAPMirrorPort ipv4Packet:tcpData
+                                                 dscp:0
+                                                flags:0
+                                                  ttl:32
+                                             protocol:6 /* TCP */
+                                        sourceAddress:@"10.10.10.10"
+                                   destinationAddress:@"20.20.20.20"
+                                               ident:0
+                                            fragment:0];
+
+        NSData *ipData2 = [UMPCAPMirrorPort ipv4Packet:udpData
+                                                 dscp:0
+                                                flags:0
+                                                  ttl:32
+                                             protocol:17 /* UDP */
+                                        sourceAddress:@"10.10.10.10"
+                                   destinationAddress:@"20.20.20.20"
+                                               ident:0
+                                            fragment:0];
+
+        NSData *ipData3 = [UMPCAPMirrorPort ipv4Packet:sctpData
+                                                 dscp:0
+                                                flags:0
+                                                  ttl:32
+                                             protocol:132 /* SCTP */
+                                        sourceAddress:@"10.10.10.10"
+                                   destinationAddress:@"20.20.20.20"
+                                               ident:0
+                                            fragment:0];
+
+
+        NSData *ethernetData1 = [UMPCAPMirrorPort ethernetPacket:ipData1
+                                               sourceMacAddress:srcMacAddr
+                                          destinationMacAddress:dstMacAddr
+                                                   ethernetType:0x0800];
+
+        NSData *ethernetData2 = [UMPCAPMirrorPort ethernetPacket:ipData2
+                                               sourceMacAddress:srcMacAddr
+                                          destinationMacAddress:dstMacAddr
+                                                   ethernetType:0x0800];
+
+        NSData *ethernetData3 = [UMPCAPMirrorPort ethernetPacket:ipData3
+                                               sourceMacAddress:srcMacAddr
+                                          destinationMacAddress:dstMacAddr
+                                                   ethernetType:0x0800];
+
+        NSLog(@"Sending data1: %@",ethernetData1.hexString);
+        NSLog(@"Sending data2: %@",ethernetData2.hexString);
+        NSLog(@"Sending data3: %@",ethernetData3.hexString);
         UMPCAPMirrorPort *con = [[UMPCAPMirrorPort alloc]init];
-        [con openDevice:@"en0"];
+        NSLog(@"con: %@",con);
+        int i = [con openDevice:@"en0"];
+        NSLog(@"openDevice(en0): %d %@",i,con.lastError);
+
+        i = [con writeEthernetPacket:ethernetData1];
+        NSLog(@"writeEthernetPacket: %d %@",i,con.lastError);
+        i = [con writeEthernetPacket:ethernetData2];
+        NSLog(@"writeEthernetPacket: %d %@",i,con.lastError);
+        i = [con writeEthernetPacket:ethernetData3];
+        NSLog(@"writeEthernetPacket: %d %@",i,con.lastError);
+
         
         
-        unsigned char ipPayload[] = { 0xc9,0x65,0x01,0xbb,0x01,0x89,0x81,0xf8,0x62,0xd9,0x34,0x66,0x80,0x18,0x10,0x00,0x18,0xd1,0x00,0x00,0x01,0x01,0x08,0x0a,0x0a,0x8b,0x92,0x38,0x62,0xbe,0x6a,0x7d };
-        NSData *ipData = [NSData dataWithBytes:ipPayload length:sizeof(ipPayload)];
-        
+        [con close];
     }
     return 0;
 }
--- ulibpcap/UMPCAPMirrorPort.h
+++ ulibpcap/UMPCAPMirrorPort.h
@@ -7,6 +7,7 @@
 //
 
 #import <ulib/ulib.h>
+#import <pcap/pcap.h>
 
 typedef enum UMPCAPMirrorPort_error
 {
@@ -27,39 +28,92 @@ typedef enum UMPCAPMirrorPort_error
     NSData      *_remoteMacAddress;
     BOOL        _verbose;
     int         _sockfd;
+    int         _snaplen;
+    int         _promisc;
+    int         _to_ms;
+    pcap_t*     _pcap;
+    NSString    *_lastError;
 }
 
 
 @property(readwrite,strong,atomic)  NSString    *name;
 @property(readwrite,strong,atomic)  NSString    *interfaceName;
+@property(readwrite,strong,atomic)  NSString    *lastError;
 @property(readwrite,assign,atomic)  int         interfaceIndex;
 @property(readwrite,assign,atomic)  int         linkNumber;
 @property(readwrite,strong,atomic)  NSData      *localMacAddress;
 @property(readwrite,strong,atomic)  NSData      *remoteMacAddress;
 @property(readwrite,assign,atomic)  BOOL        verbose;
 @property(readwrite,assign,atomic)  int         sockfd;
+@property(readwrite,assign,atomic)  int         snaplen;
+@property(readwrite,assign,atomic)  int         promisc;
+@property(readwrite,assign,atomic)  int         to_ms;
 
-- (UMPCAPMirrorPort_error)openDevice;
-- (UMPCAPMirrorPort_error)openDevice:(NSString *)deviceName;
+
+
+- (UMPCAPMirrorPort *)initWithLinkNumber:(int)linkNumber;
 + (NSData *)macAddressFromString:(NSString *)in;
 - (void)setConfig:(NSDictionary *)dict;
 
-- (void)writeFromMacAddress:(NSData *)srcMac
-               toMacAddress:(NSData *)dstMac
-               ethernetType:(int)ethType
-                       dscp:(int)dscp
-                      flags:(int)flags
-                        ttl:(int)ttl
-                   protocol:(int)protocol
-              sourceAddress:(NSString *)srcIp
-         destinationAddress:(NSString *)dstIp
-                 sourcePort:(int)srcPort
-            destinationPort:(int)dstPort
-                       data:(NSData *)data;
+- (UMPCAPMirrorPort_error)openDevice;
+- (UMPCAPMirrorPort_error)openDevice:(NSString *)deviceName;
+- (void)close;
+
+- (int)writeEthernetPacket:(NSData *)payload;
+
++ (NSData *)ethernetPacket:(NSData *)payload
+          sourceMacAddress:(NSData *)srcMacAddr
+     destinationMacAddress:(NSData *)dstMacAddr
+              ethernetType:(uint16_t)ethType;
+
++ (NSData *)ipv4Packet:(NSData *)data
+                  dscp:(uint8_t)dscp
+                 flags:(uint8_t)flags
+                   ttl:(uint8_t)ttl
+              protocol:(uint8_t)protocol
+         sourceAddress:(NSString *)srcIp
+    destinationAddress:(NSString *)dstIp
+                 ident:(int)ident
+              fragment:(int)fragment;
+
++ (NSData *)tcpPacket:(NSData *)tcpPayload
+        sourceAddress:(NSString *)srcIp
+   destinationAddress:(NSString *)dstIp
+           sourcePort:(uint16_t)sourcePort
+      destinationPort:(uint16_t)destinationPort
+       sequenceNumber:(uint32_t)seq
+            ackNumber:(uint32_t)ack
+                flags:(uint16_t)flags
+           windowSize:(uint16_t)windowSize
+        urgentPointer:(uint16_t)urgentPointer
+       fragmentLength:(uint16_t)fragmentLength;
+
+
++ (NSData *)udpPacket:(NSData *)udpPayload
+        sourceAddress:(NSString *)srcIp
+   destinationAddress:(NSString *)dstIp
+           sourcePort:(uint16_t)sourcePort
+      destinationPort:(uint16_t)destinationPort;
+//       fragmentLength:(uint16_t)fragmentLength;
+
++ (NSData *)sctpChunk:(NSData *)payload
+                flags:(uint8_t)flags
+                  tsn:(uint32_t)tsn
+               stream:(uint16_t)stream
+ streamSequenceNumber:(uint16_t)streamSequence
+   protocolIdentifier:(uint32_t)pid;
+
++ (NSData *)sctpPacket:(NSArray<NSMutableData *>*)chunks
+            sourcePort:(uint16_t)sourcePort
+       destinationPort:(uint16_t)destinationPort
+       verificationTag:(uint32_t)ver;
 
 + (uint16_t)  layer4_checksum:(NSData *)payload
-                    headerPtr:(uint8_t *)h
-                    headerLen:(int)headerLen;
-
+                    headerPtr:(uint8_t *)headerPtr
+                    headerLen:(int)headerLen
+                     sourceIp:(NSString *)sourceIP
+                destinationIp:(NSString *)destinationIP
+                     protocol:(uint8_t)protocol
+               fragmentLength:(uint16_t)fragmentLength;
 
 @end
